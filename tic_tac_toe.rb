33be083cd90d137250c43363a17be59f79624a48
Martin <martinochwat1@gmail.com>
2015-01-21 14:01:04 -0500

    Updated look of board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -17,12 +17,19 @@ class Board
 	def initialize(player1, player2)
 		@player1 = Player.new(player1, 'X')
 		@player2 = Player.new(player2, 'O')
-		@board = [[0,1,"O"],[3,"O",5],[6,7,8]]
+		@board = [[0,1,2],[3,4,5],[6,7,8]]
 	end
 
-	def show_board
-		@board.each { |row| p row }
-	end
+	# Show the Tic Tac Board
+	def show_board                 
+    puts
+    puts " #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]}"
+    puts "-----------"
+    puts " #{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]}"
+    puts "-----------"
+    puts " #{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]}"
+    puts
+  	end
 
 	def go(player, token)
 		puts "#{player} choose a spot on the board by typing in its number:"
